3b4d1e8b9679221312c20929435470ea94be91a2
PhamVanPhuc2k2 <phamvanphuctnp@gmail.com>
2025-03-20 09:31:28 +0700

    Kết nối tới MySQL Database với Sequelize

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,15 @@
-const getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+import db from '../models/index';
+
+const getHomePage = async (req, res) => {
+    try {
+        const data = await db.User.findAll();
+
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data),
+        });
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 export default {
